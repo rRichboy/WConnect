40d2fa5087d22b9b5c3426f210dc249f0016a712
rRichboy <tvinktvink893@gmail.com>
2024-01-22 10:03:01 +0300

    Update any

--- app/src/main/java/com/example/myapplication/SignIn.java
+++ app/src/main/java/com/example/myapplication/SignIn.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignIn extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class SignIn extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
     }
+
+    public void moveOn2(View view) {
+        Intent intent = new Intent(SignIn.this, SignUp.class);
+        SignIn.this.startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/myapplication/SignUp.java
+++ app/src/main/java/com/example/myapplication/SignUp.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUp extends AppCompatActivity {
 

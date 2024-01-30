6667e5d730543c825f7fa8d99782c4d38d3b039f
rRichboy <tvinktvink893@gmail.com>
2024-01-30 16:26:00 +0300

    Передача данных между Activity

--- app/src/main/java/com/example/myapplication/SignIn.java
+++ app/src/main/java/com/example/myapplication/SignIn.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class SignIn extends AppCompatActivity {
 
@@ -12,11 +13,27 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        EditText email = findViewById(R.id.editTextTextEmailAddress1);
+        EditText password = findViewById(R.id.editTextTextPassword1);
+
+        Bundle args = getIntent().getExtras();
+
+        if (args != null) {
+
+            String emailArg = args.getString("email");
+            String passwordArg = args.getString("password");
+
+            email.setText(emailArg);
+            password.setText(passwordArg);
+        }
     }
 
     public void moveOn2(View view) {
         Intent intent = new Intent(SignIn.this, SignUp.class);
         SignIn.this.startActivity(intent);
     }
+
+
 
 }
--- app/src/main/java/com/example/myapplication/SignUp.java
+++ app/src/main/java/com/example/myapplication/SignUp.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class SignUp extends AppCompatActivity {
 
@@ -18,5 +19,20 @@ public class SignUp extends AppCompatActivity {
         Intent intent = new Intent(SignUp.this, SignIn.class);
         SignUp.this.startActivity(intent);
     }
+
+    public void sign_up(View view){
+        EditText email = findViewById(R.id.editTextTextEmailAddress1);
+        EditText password = findViewById(R.id.editTextTextPassword1);
+
+        String emailStr = email.getText().toString();
+        String passwordStr = password.getText().toString();
+
+        Intent intent = new Intent(this, SignIn.class);
+        intent.putExtra("email", emailStr);
+        intent.putExtra("password", passwordStr);
+        startActivity(intent);
+
+    }
+
 
 }
